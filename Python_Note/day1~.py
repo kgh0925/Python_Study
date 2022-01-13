6bb99d7dd0aa2739210dcdf5526abdffeb4c9a9d
kim geunho <96340564+kgh0925@users.noreply.github.com>
2022-01-13 13:25:14 +0000

    Studying about the list.

--- Python_Note/day1~.py
+++ Python_Note/day1~.py
@@ -43,4 +43,31 @@ while index != -1 :
   i = i + 1
   index = test1.find("A", + i)
   print(i)
+
+[day2]
+
+url = "http://youtube.com"
+my_str = url.replace("http://", "")
+
+my_str = my_str[:my_str.index(".")]
+print(my_str)
+
+word = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+
+print(word)
+
+subway = ["A","B","C","A","A"]
+print(subway)
+print(subway.index("A"))
+print(subway[1])
+print(subway.count("A"))
+print(subway.pop())
+print(subway)
+print(subway.count("A"))
+subway.sort()
+print(subway)
+subway.reverse()
+print(subway)
+subway.clear()
+print(subway)
 """
